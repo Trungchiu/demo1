648b2fd2574b4a3fce823b5fc0f2f6248f0a45c0
Trungchiu <145974456+Trungchiu@users.noreply.github.com>
2023-09-25 16:46:39 +0700

    init project

---
+++ Assets/RW/Scripts/MusicControl.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace RayWenderlich.SpaceInvadersUnity
+{
+    public class MusicControl : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource source;
+
+        internal void StopPlaying() => source.Stop();
+    }
+}
